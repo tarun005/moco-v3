d59ef67f8aec3611749696dd11f2ad6bce3442fd
Tarun <tannu.2354@gmail.com>
2023-03-29 10:07:11 -0700

    Chaned dl

--- main_moco.py
+++ main_moco.py
@@ -297,7 +297,7 @@ def main_worker(gpu, ngpus_per_node, args):
     cudnn.benchmark = True
 
     # Data loading code
-    traindir = os.path.join(args.data, 'train')
+    traindir = os.path.join(args.data)
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
